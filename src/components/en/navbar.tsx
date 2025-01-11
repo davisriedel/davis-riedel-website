ddd3f344ac95f47285dce7ae8ea30134dc01d3ae
Davis Riedel <mail@davis-riedel.de>
2025-01-11 22:06:02 +0100

    remove blog from navbar until content added

--- src/components/en/navbar.tsx
+++ src/components/en/navbar.tsx
@@ -8,10 +8,6 @@ export function NavBar() {
 					Home
 				</AnimatedLink>
         <span> | </span>
-				<AnimatedLink className="text-white after:bg-white dark:text-white dark:after:bg-white" href="/en/posts">
-					Blog
-				</AnimatedLink>
-        <span> | </span>
 				<AnimatedLink className="text-white after:bg-white dark:text-white dark:after:bg-white" href="/en/contact">
 					Contact
 				</AnimatedLink>
@@ -20,3 +16,7 @@ export function NavBar() {
 	);
 }
 
+// <span> | </span>
+// <AnimatedLink className="text-white after:bg-white dark:text-white dark:after:bg-white" href="/en/posts">
+// 	Blog
+// </AnimatedLink>
